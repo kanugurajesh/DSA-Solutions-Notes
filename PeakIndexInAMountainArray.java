2245e2ec70399173c238c507e7356f04da563ce8
kanugu rajesh <77529419+kanugurajesh@users.noreply.github.com>
2024-06-21 15:39:02 +0530

    Update PeakIndexInAMountainArray.java

--- PeakIndexInAMountainArray.java
+++ PeakIndexInAMountainArray.java
@@ -1,9 +1,8 @@
 class Solution {
-
+    
     public int binarySearch(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
-
         while (start <= end) {
             int mid = start + (end - start)/2;
             if ((end - start) == 1) {
@@ -14,9 +13,21 @@ class Solution {
             else if (nums[mid] < nums[mid-1]) end = mid - 1;
             else start = mid + 1;
         }
-
         return -1;
+    }
 
+    // Optimal solution
+
+    public int optimalSearch(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] > arr[mid + 1]) end = mid;
+            else start = mid + 1;
+        }
+        // you return start or end because at the end both of them are same
+        return start;
     }
 
     public int peakIndexInMountainArray(int[] arr) {
